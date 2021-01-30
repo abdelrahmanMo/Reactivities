1f52e081bbc1ee2938142458aa09309b61df6914
abdelrahmanMo <abdo.mohamed.am.ao@gmail.com>
2021-01-31 00:51:58 +0200

    add mobx to client project

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,97 +1,28 @@
-import React, { SyntheticEvent, useEffect, useState } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Container } from 'semantic-ui-react'
-import { IActivity } from '../models/activity'
 import NavBar from '../../features/nav/NavBar'
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard'
-import agent from '../api/agent';
 import LoadingComponent from './LoadingComponent';
+import ActivityStore from '../stores/activityStore';
+import { observer } from 'mobx-react-lite';
 
 
 const App = () => {
 
-  const [activities, setActivities] = useState<IActivity[]>([])
-  const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(null);
-  const [editMode, setEditMode] = useState(false);
-  const [loading, setLoading] = useState(true);
-  const [submitting, setSubmitting] = useState(false)
-  const [target,setTarget] = useState('');
-
+  const activityStore = useContext(ActivityStore)
   useEffect(() => {
-    agent.Activities.list()
-      .then(response => {
-        let activities: IActivity[] = [];
-        response.forEach((activity) => {
-          activity.date = activity.date.split('.')[0];
-          activities.push(activity);
-        })
-        setActivities(activities);
-      }).then(() => setLoading(false))
-
-  }, [])
-
-  const handleSelectActivity = (id: string) => {
-    setSelectedActivity(activities.filter(a => a.id === id)[0]);
-    setEditMode(false);
-  }
-
-  const handleOpenCreateForm = () => {
-    setEditMode(true);
-    setSelectedActivity(null);
-  }
-
-  const handleCreateActivity = (activity: IActivity) => {
-    setSubmitting(true);
-    agent.Activities.create(activity).then(() => {
-      setActivities([...activities, activity]);
-      setSelectedActivity(activity);
-      setEditMode(false);
-    }).then(() => setSubmitting(false))
-
-  }
-
-  const handleEditActivity = (activity: IActivity) => {
-    setSubmitting(true);
-    agent.Activities.update(activity).then(() => {
-      setActivities([...activities.filter(a => a.id !== activity.id), activity])
-      setSelectedActivity(activity);
-      setEditMode(false);
-    }).then(() => setSubmitting(false))
-
-
-  }
-
-  const handleDeleteAcvtivity = (event:SyntheticEvent<HTMLButtonElement>,id: string) => {
-    setSubmitting(true);
-    setTarget(event.currentTarget.name)
-    agent.Activities.delete(id).then(() => {
-      setActivities([...activities.filter(a => a.id !== id)]);
-    }).then(() => setSubmitting(false));
-
-  }
-
-  if (loading) return <LoadingComponent content='Loading activities...' />;
-
-
+    activityStore.loadActivities();
+  }, [activityStore])
+  if (activityStore.loadingInitial) return <LoadingComponent content='Loading activities...' />;
   return (
     <>
 
-      <NavBar openCreateForm={handleOpenCreateForm} />
+      <NavBar />
       <Container className='list__container'>
-        <ActivityDashboard activities={activities}
-          selectActivity={handleSelectActivity}
-          selectedActivity={selectedActivity}
-          editMode={editMode}
-          setEditMode={setEditMode}
-          setSelectedActivity={setSelectedActivity}
-          createActivity={handleCreateActivity}
-          editActivity={handleEditActivity}
-          deleteActivity={handleDeleteAcvtivity}
-          submitting={submitting}
-          target={target}
-        />
+        <ActivityDashboard />
       </Container>
     </>
   );
 }
 
-export default App;
+export default observer(App);
--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,30 +1,30 @@
-import React from 'react'
-import { Button, Card, Image } from 'semantic-ui-react'
-import { IActivity } from '../../../app/models/activity'
+import { observer } from 'mobx-react-lite';
+import React, { useContext } from 'react';
+import { Button, Card, Image } from 'semantic-ui-react';
+import ActivityStore from '../../../app/stores/activityStore';
 
-interface IProps {
-    activity: IActivity,
-    setEditMode:(editMode:boolean)=>void;
-    setSelectedActivity:(activity:IActivity | null)=>void,
-}
-const ActivityDetails: React.FC<IProps> = ({ activity,setEditMode,setSelectedActivity }) => {
+
+const ActivityDetails: React.FC = ( ) => {
+    const activityStore = useContext(ActivityStore);
+    const {selectedActivity:activity,openEditForm,cancelSelectedActivity} = activityStore;
+
     return (
         <>
             <Card fluid>
-                <Image src={`/assets/categoryImages/${activity.category}.jpg`} wrapped ui={false} />
+                <Image src={`/assets/categoryImages/${activity!.category}.jpg`} wrapped ui={false} />
                 <Card.Content>
-                    <Card.Header>{activity.title}</Card.Header>
+                    <Card.Header>{activity!.title}</Card.Header>
                     <Card.Meta>
-                        <span className='date'>{activity.date}</span>
+                        <span className='date'>{activity!.date}</span>
                     </Card.Meta>
                     <Card.Description>
-                        {activity.description}
+                        {activity!.description}
                     </Card.Description>
                 </Card.Content>
                 <Card.Content extra>
                     <Button.Group widths={2}>
-                        <Button onClick={()=>setEditMode(true)} basic color='blue' content='Edit'></Button>
-                        <Button basic color='blue' onClick={()=>setSelectedActivity(null)} content='Cancel'></Button>
+                        <Button onClick={() => openEditForm(activity!.id)} basic color='blue' content='Edit'></Button>
+                        <Button basic color='blue' onClick={cancelSelectedActivity} content='Cancel'></Button>
                     </Button.Group>
                 </Card.Content>
             </Card>
@@ -32,4 +32,4 @@ const ActivityDetails: React.FC<IProps> = ({ activity,setEditMode,setSelectedAct
     )
 }
 
-export default ActivityDetails
+export default observer(ActivityDetails);
